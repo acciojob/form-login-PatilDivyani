3370ba328666559102196f3d001620a6f802f7be
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-19 16:40:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+    var firstName = document.getElementById("fname").value;
+  var lastName = document.getElementById("lname").value;
 
+  // Print the first and last name using alert
+  alert( firstName + lastName);
 }
+
+let submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", getFormvalue);
