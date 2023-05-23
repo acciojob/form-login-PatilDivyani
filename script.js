466e84e8a00e35c704bd196a4926e7f5d08de938
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-23 20:08:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function getFormvalue() {
     //Write your code here
     let text = document.getElementsByTagName("input");
-    let firstName = text[0].value;
-    let lastName = text[1].value;
+    let firstName = text[0].value.trim();
+    let lastName = text[1].value.trim();
 
   // Print the first and last name using alert
-  alert( firstName + lastName);
+  alert( firstName +" " + lastName);
 }
 
-let submitButton = document.getElementById("submit");
-submitButton.addEventListener("click", getFormvalue);
+// let submitButton = document.getElementById("submit");
+// submitButton.addEventListener("click", getFormvalue);
